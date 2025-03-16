c5b4aa894a25e0261729b48b37e55b8a27fde2f1
netai-kara-ii <netai.kara.ii@gmail.com>
2025-03-16 18:03:20 +0000

    vectorised normal cdf and icdf

--- include/normal-distribution.h
+++ include/normal-distribution.h
@@ -7,21 +7,41 @@
 
 #include "types.h"
 
-
 template <typename T>
 class StandardGaussian final {
+
+    /*
+     * Vectorised cdf and icdf for the standard Gaussian (zero mean,
+     * unit variance) distribution. 
+    */
+
     public:
-        ndarray<T> cdf(ndarray<T> x) {
-            return (T)0.5 * ((T)1 + erf(x * INV_SQRT_TWO));
-        }
+        ndarray<T> cdf(ndarray<T> x) { 
+
+            /*
+             * Args: a vector of points x \in R^n, of any real type.
+             *
+             * Performs element-wise cumulative distribution function
+             * in terms of the erf. Uses built-in Eigen function.
+            */
+
+            return (T)0.5 * ((T)1 + erf(x * INV_SQRT_TWO)); }
 
         ndarray<T> icdf(ndarray<T> p) {
-            ndarray<T> t = (T)2 * p - (T)1;
-            t = t.unaryExpr(
-                [&](T e) { return std::erf_inv(e); }, Eigen::Vectorized()
-            ); 
 
-            return SQRT_TWO * t;
+            /*
+             * Args: a vector of probabilities p, therefore verifying 
+             * 0 <= p_i <= 1 for every i = 1, ..., N, of any real type.
+             * 
+             * Performs element-wise inverse cummulative distribution 
+             * function (quantile), in terms of the inverse erf. Passes
+             * a capture-less lambda that shifts and scales arguments 
+             * to fit Gaussian dist. The initial idea was to use a 
+             * function pointer but Eigen does not like it.
+            */
+
+            return p.unaryExpr([](T e) { return SQRT_TWO * std::erf_inv((T)2 * e - (T)1); }, 
+                        Eigen:A:Vectorized()); 
         }
          
        
@@ -36,6 +56,10 @@ constexpr T StandardGaussian<T>::INV_SQRT_TWO = static_cast<T>(std::numbers::inv
 
 template <typename T>
 constexpr T StandardGaussian<T>::SQRT_TWO = static_cast<T>(std::numbers::sqrt2);
+
+
+// to do: test and benchmark. Particularly return value optimizations, and check for 
+// speedups with vectorization over boost (baseline).
 
 
 #endif
